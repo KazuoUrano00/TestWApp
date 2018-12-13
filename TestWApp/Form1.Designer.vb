dc90291672703fd2e036e42899c60eafbac6da9f
浦野 一男 <urano.kazuo@opst.co.jp>
2018-12-13 17:17:08 +0900

    プロジェクト ファイルを追加します。

---
+++ TestWApp/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(81, 343)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "表示"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(112, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(85, 19)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(203, 28)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(85, 19)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(294, 28)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(85, 19)
+        Me.TextBox3.TabIndex = 4
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(385, 28)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(85, 19)
+        Me.TextBox4.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(115, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 12)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "FirstName"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(206, 13)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 12)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "LastName"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(294, 13)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 12)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Birthday"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(385, 13)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 12)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Gender"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(162, 343)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "登録"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(22, 13)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(55, 12)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "StudentID"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(19, 28)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(85, 19)
+        Me.TextBox5.TabIndex = 1
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(243, 343)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "更新"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(324, 343)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 13
+        Me.Button4.Text = "削除"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(19, 72)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 21
+        Me.DataGridView1.Size = New System.Drawing.Size(450, 265)
+        Me.DataGridView1.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(482, 390)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ TestWApp/Form1.vb
@@ -0,0 +1,262 @@
+﻿Imports System.Data.Common
+Imports System.Data.OleDb
+Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim prov As DbProviderFactory = Nothing
+        Dim con As DbConnection = Nothing
+        Dim cmd As DbCommand = Nothing
+
+        'False:Oracle接続、True:SQLServer接続
+        Dim isDbSQLServer As Boolean = True
+        Dim strProvider As String
+        Dim strConnection As String
+
+        'DbProviderFactoryオブジェクトの生成
+        If isDbSQLServer Then
+            'SQLServer接続の場合
+            strProvider = "System.Data.SqlClient"
+            strConnection = String.Format("Server=DESKTOP-HMSGQGP\SQLEXPRESS;Database=Test;Integrated Security=True;",
+            "192.168.1.1",
+            "sqlDB")
+
+        Else
+            'Oracle接続の場合
+            strProvider = "Oracle.DataAccess.Client"
+            strConnection = String.Format("Data Source={0};User Id={1};Password={2};",
+            "oraDB",
+            "userid",
+            "userpw")
+        End If
+
+        prov = DbProviderFactories.GetFactory(strProvider)
+
+        'DbConnectionオブジェクトの生成、及び、オープン
+        con = prov.CreateConnection()
+        con.ConnectionString = strConnection
+        con.Open()
+        '---------
+
+        'DBCommandオブジェクトの生成、及び、コネクションの設定
+        cmd = prov.CreateCommand()
+        cmd.Connection = con
+
+        Dim strSql As New System.Text.StringBuilder()
+        Dim param As DbParameter = Nothing
+        Dim adapter As DbDataAdapter = Nothing
+        Dim ds As New DataTable()
+
+        'SQL 文の生成
+        strSql.AppendLine("SELECT * ")
+        strSql.AppendLine("FROM Students")
+
+        '実行する SQL 文を設定
+        cmd.CommandText = strSql.ToString
+
+        'パラメーターの初期化
+        cmd.Parameters.Clear()
+
+        '変換パラメーターの設定
+        'param = prov.CreateParameter()
+        'param.DbType = DbType.Int32
+        'param.ParameterName = "PERSON_NUM"
+        'param.Value = 5
+        'cmd.Parameters.Add(param)
+        '
+        'param = prov.CreateParameter()
+        'param.DbType = DbType.String
+        'param.ParameterName = "DEPT_ID_STR"
+        'param.Value = "A100-01"
+        'cmd.Parameters.Add(param)
+
+        'アダプターの生成、及び、SQL文の発行
+        adapter = prov.CreateDataAdapter()
+        adapter.SelectCommand = cmd
+        adapter.Fill(ds)
+
+        MsgBox(ds.Rows.Count)
+
+        DataGridView1.Columns.Add("StudentID", "StudentID")
+        DataGridView1.Columns.Add("FirstName", "FirstName")
+        DataGridView1.Columns.Add("LastName", "LastName")
+        DataGridView1.Columns.Add("Birthday", "Birthday")
+        DataGridView1.Columns.Add("Gender", "Gender")
+
+        For Each row As DataRow In ds.Rows
+            DataGridView1.Rows.Add(row(0), row(1), row(2), row(3), row(4))
+        Next
+
+        ' DB接続を閉じる
+        If Not con Is Nothing Then
+            con.Close()
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim strProvider As String
+        Dim strConnection As String
+        Dim prov As DbProviderFactory = Nothing
+        Dim con As DbConnection = Nothing
+        Dim cmd As SqlCommand = Nothing
+        Dim strSql As New System.Text.StringBuilder()
+        Dim adapter2 As DbDataAdapter = Nothing
+        Dim ds2 As New DataTable()
+
+        'SQLServer接続の場合
+        strProvider = "System.Data.SqlClient"
+        strConnection = String.Format("Server=DESKTOP-HMSGQGP\SQLEXPRESS;Database=Test;Integrated Security=True;",
+            "192.168.1.1",
+            "sqlDB")
+
+        prov = DbProviderFactories.GetFactory(strProvider)
+
+        'DbConnectionオブジェクトの生成、及び、オープン
+        con = prov.CreateConnection()
+        con.ConnectionString = strConnection
+        con.Open()
+
+        'SQL 文の生成
+        strSql.AppendLine("SELECT * ")
+        strSql.AppendLine("FROM Students")
+
+        'DBCommandオブジェクトの生成、及び、コネクションの設定
+        cmd = prov.CreateCommand()
+        cmd.Connection = con
+
+        '実行する SQL 文を設定
+        cmd.CommandText = strSql.ToString
+
+        'パラメーターの初期化
+        cmd.Parameters.Clear()
+
+        'アダプターの生成、及び、SQL文の発行
+        adapter2 = prov.CreateDataAdapter()
+        adapter2.SelectCommand = cmd
+        adapter2.Fill(ds2)
+
+        ''実行する SQL 文を設定
+        'cmd.CommandText = strSql.ToString
+
+        ' SQL文の設定
+        cmd.CommandText = "INSERT INTO Students (StudentID,FirstName,LastName,Birthday,Gender) VALUES(@temp0,@temp1,@temp2,@temp3,@temp4)"
+
+        ' パラメータの設定
+        cmd.Parameters.Add(New SqlParameter("@temp0", SqlDbType.Int)).Value = ds2.Rows.Count + 1
+        cmd.Parameters.Add(New SqlParameter("@temp1", SqlDbType.NChar)).Value = TextBox1.Text
+        cmd.Parameters.Add(New SqlParameter("@temp2", SqlDbType.NChar)).Value = TextBox2.Text
+        cmd.Parameters.Add(New SqlParameter("@temp3", SqlDbType.Date)).Value = TextBox3.Text
+        cmd.Parameters.Add(New SqlParameter("@temp4", SqlDbType.NChar)).Value = TextBox4.Text
+
+        cmd.ExecuteReader()
+
+        MsgBox("追加成功")
+
+        ' DB接続を閉じる
+        If Not con Is Nothing Then
+            con.Close()
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim strProvider As String
+        Dim strConnection As String
+        Dim prov As DbProviderFactory = Nothing
+        Dim con As DbConnection = Nothing
+        Dim cmd As SqlCommand = Nothing
+        Dim count As Integer
+
+        'SQLServer接続の場合
+        strProvider = "System.Data.SqlClient"
+        strConnection = String.Format("Server=DESKTOP-HMSGQGP\SQLEXPRESS;Database=Test;Integrated Security=True;",
+            "192.168.1.1",
+            "sqlDB")
+
+        prov = DbProviderFactories.GetFactory(strProvider)
+
+        'DbConnectionオブジェクトの生成、及び、オープン
+        con = prov.CreateConnection()
+        con.ConnectionString = strConnection
+        con.Open()
+
+        ' コネクションの設定
+        cmd = prov.CreateCommand()
+        cmd.Connection = con
+
+        ' SQL文の設定
+        cmd.CommandText = "UPDATE Students "
+        cmd.CommandText &= "SET FirstName =@temp1, "
+        cmd.CommandText &= "LastName =@temp2, "
+        cmd.CommandText &= "Birthday =@temp3, "
+        cmd.CommandText &= "Gender =@temp4 "
+        cmd.CommandText &= "WHERE StudentID =@temp0"
+
+        ' パラメータの設定
+        cmd.Parameters.Add(New SqlParameter("@temp0", SqlDbType.Int)).Value = TextBox5.Text
+        cmd.Parameters.Add(New SqlParameter("@temp1", SqlDbType.NChar)).Value = TextBox1.Text
+        cmd.Parameters.Add(New SqlParameter("@temp2", SqlDbType.NChar)).Value = TextBox2.Text
+        cmd.Parameters.Add(New SqlParameter("@temp3", SqlDbType.Date)).Value = TextBox3.Text
+        cmd.Parameters.Add(New SqlParameter("@temp4", SqlDbType.NChar)).Value = TextBox4.Text
+
+        count = cmd.ExecuteNonQuery()
+
+        If count > 0 Then
+            ' 更新成功
+            MsgBox("更新成功")
+        Else
+            ' 更新するレコードがなかった時の処理
+            MsgBox("更新なし")
+        End If
+
+        ' DB接続を閉じる
+        If Not con Is Nothing Then
+            con.Close()
+        End If
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim strProvider As String
+        Dim strConnection As String
+        Dim prov As DbProviderFactory = Nothing
+        Dim con As DbConnection = Nothing
+        Dim cmd As SqlCommand = Nothing
+        Dim count As Integer
+
+        'SQLServer接続の場合
+        strProvider = "System.Data.SqlClient"
+        strConnection = String.Format("Server=DESKTOP-HMSGQGP\SQLEXPRESS;Database=Test;Integrated Security=True;",
+            "192.168.1.1",
+            "sqlDB")
+
+        prov = DbProviderFactories.GetFactory(strProvider)
+
+        'DbConnectionオブジェクトの生成、及び、オープン
+        con = prov.CreateConnection()
+        con.ConnectionString = strConnection
+        con.Open()
+
+        ' コネクションの設定
+        cmd = prov.CreateCommand()
+        cmd.Connection = con
+
+        ' SQL文の設定
+        cmd.CommandText = "DELETE FROM Students "
+        cmd.CommandText &= "WHERE StudentID=@temp0 "
+
+        ' SQL文パラメータの設定
+        cmd.Parameters.Add(New SqlParameter("@temp0", SqlDbType.Int)).Value = TextBox5.Text
+
+        count = cmd.ExecuteNonQuery()
+        If count > 0 Then
+            ' レコードの削除が成功した時の処理
+            MsgBox("削除成功")
+        Else
+            ' 削除するレコードがなかった時の処理
+            MsgBox("削除なし")
+        End If
+    End Sub
+
+End Class
